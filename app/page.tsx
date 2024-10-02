3571c8dfb20d50d7c954123dc3f6b4c5a8bdac9c
abhay.prajapati <abhay.prajapati@deskera.org>
2024-10-02 12:25:53 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import React from "react";
 import Editor from "./components/Editor";
 
 export default function Home() {
-  return <Editor />;
+  return (
+    <>
+      <Editor />
+      <footer className="w-full bg-gray-900 text-gray-400 py-4  bottom-0">
+      <div className="container mx-auto flex justify-center gap-4 items-center px-4">
+        <a
+          href="https://github.com/theabhayprajapati/ende-forge"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-cyan-400 transition-colors duration-300"
+        >
+          @github
+        </a>
+
+        <a
+          href="https://x.com/abhayprajapati_"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-cyan-400 transition-colors duration-300"
+        >
+          @abhayprajapati_
+        </a>
+      </div>
+    </footer>
+    </>
+  );
 }
